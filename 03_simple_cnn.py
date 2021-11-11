60a7302aa14ad135e01c99a18f1eed736bf89850
Manoj Kannan <71482820+Manoj-Kannan@users.noreply.github.com>
2021-11-11 07:50:05 +0530

    dataset_loaded

---
+++ 03_simple_cnn.py
@@ -0,0 +1,16 @@
+# Imports
+import torch
+import torchvision # torch package for vision related things
+import torch.nn.functional as F  # Parameterless functions, like (some) activation functions
+import torchvision.datasets as datasets  # Standard datasets
+import torchvision.transforms as transforms  # Transformations we can perform on our dataset for augmentation
+from torch import optim  # For optimizers like SGD, Adam, etc.
+from torch import nn  # All neural network modules
+from torch.utils.data import DataLoader  # Gives easier dataset managment by creating mini batches etc.
+from tqdm import tqdm  # For nice progress bar!
+
+# Load Training and Test data
+train_dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root="dataset/", train=False, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
