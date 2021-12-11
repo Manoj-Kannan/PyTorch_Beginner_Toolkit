07226ead67a1c764b4c1d246a6d51765895b4abf
Manoj Kannan <71482820+Manoj-Kannan@users.noreply.github.com>
2021-12-11 17:59:10 +0530

    get_statedict

--- 04_loadsave_basics.py
+++ 04_loadsave_basics.py
@@ -34,3 +34,17 @@ class TheModelClass(nn.Module):
 
 # Initialize model
 model = TheModelClass()
+
+# Initialize optimizer
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Print model's state_dict
+print("Model's state_dict:")
+for param_tensor in model.state_dict():
+    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+# Print optimizer's state_dict
+print("Optimizer's state_dict:")
+for var_name in optimizer.state_dict():
+    print(var_name, "\t", optimizer.state_dict()[var_name])
+#---------------------------------------------------------------------#
